e4ef5fd8e7010f658957f951ee5a29e06a97d82b
nakunxuanviet <nakunxuanviet@gmail.com>
2019-05-01 22:56:16 +0700

    add thuc cau cau trucIf Else

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,4 +1,25 @@
-function calc(){
-    var acx = document.calc.number1.value * document.calc.number2.value;
-    elert(acx);
+function addition(){
+    // document.getElementById("number1").value = parseInt(document.getElementById("number1").value);
+    // document.getElementById("number2").value = parseInt(document.getElementById("number2").value);
+    x = document.getElementById("number1").value;
+    y = document.getElementById("number2").value;
+    document.getElementById('result').innerHTML ="Result Addition: "+ x + y;
+}
+
+function subtraction(){
+    x = document.getElementById("number1").value;
+    y = document.getElementById("number2").value;
+    document.getElementById('result').innerHTML ="Result Subtraction: " + x - y;
+}
+
+function multiplication(){
+    x = document.getElementById("number1").value;
+    y = document.getElementById("number2").value;
+    document.getElementById('result').innerHTML ="Result Multiplication: "+ x * y;
+}
+
+function division(){
+    x = document.getElementById("number1").value;
+    y = document.getElementById("number2").value;
+    document.getElementById('result').innerHTML ="Result Division: "+ x / y;
 }
