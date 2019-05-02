7c7a411c37d9c29349dd987951739a3544153b77
nakunxuanviet <nakunxuanviet@gmail.com>
2019-05-02 14:40:51 +0700

    add kiem tra nam nhuan

--- ChuyenDoiTienTe2/chuyendoitiente.js
+++ ChuyenDoiTienTe2/chuyendoitiente.js
@@ -4,39 +4,44 @@ function converter(){
     let z = document.getElementById('toCurrency').value;
     let inputvl = parseInt(x);
     
-    if (y == "VND") {
-        if (z == "VND") {
-            document.getElementById("result").innerHTML = inputvl;
-        } else {
-            if (z == "USD") {
-                document.getElementById("result").innerHTML = inputvl / 20000;
-            } else document.getElementById("result").innerHTML = inputvl / 30000;
-        }
-    }
-
-    if (y == "USD") {
-        if (z == "VND") {
-            document.getElementById("result").innerHTML = inputvl * 20000;
-        } else {
-            if (z == "USD") {
-                document.getElementById("result").innerHTML = inputvl;
-            } else {
-                document.getElementById("result").innerHTML = inputvl * 2 / 3;
+    function converter(){
+        let x = document.getElementById('inputnumber').value;
+        let y = document.getElementById('fromCurrency').value;
+        let z = document.getElementById('toCurrency').value;
+        let inputvl = parseInt(x);
+        
+        if (y == "vnd"){
+            if ( z == "vnd"){
+                document.getElementById('result').innerHTML = inputvl;
             }
-            if (y == "EUR") {
-                if (z == "VND") {
-                    document.getElementById("result").innerHTML = inputvl * 30000;
-                } else {
-                    if (z == "USD") {
-                        document.getElementById("result").innerHTML = inputvl * 3 / 2;
-                    } else document.getElementById("result").innerHTML = inputvl ;
-
-
-                }
+            else if(z == "usd"){
+                document.getElementById('result').innerHTML = inputvl /23000;
+            }else{
+                document.getElementById('result').innerHTML = inputvl /26000;
+            }
+        }else if(y == "usd"){
+            if (z == "usd"){
+                document.getElementById('result').innerHTML = inputvl;
+            }
+            else if (z == "vnd"){
+                document.getElementById('result').innerHTML = inputvl * 23000;
+            }else{
+                document.getElementById('result').innerHTML = inputvl * (23000/26000);
+            }
+        }else{
+            if (z == "eur"){
+                document.getElementById('result').innerHTML = inputvl;
+            }else if(z == "usd"){
+                document.getElementById('result').innerHTML = inputvl * (26000/23000);
+            }else{
+                document.getElementById('result').innerHTML = inputvl * 26000;
             }
         }
-    }
-}
+    
+        
+            
+    
+    
 
     
         
